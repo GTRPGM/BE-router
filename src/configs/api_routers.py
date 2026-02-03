3806dcec7208054f9b4169a512f34488b32a3553
elian118 <elian118@naver.com>
2026-02-03 16:37:40 +0900

    feat: add gm_routers

--- src/configs/api_routers.py
+++ src/configs/api_routers.py
@@ -1,11 +1,13 @@
 from src.auth.auth_router import auth_router
 from src.state.state_router import state_router
+from src.gm.gm_routers import gm_router
 from info.info_router import info_router
 from src.minigame.minigame_router import minigame_router
 
 API_ROUTERS = [
     auth_router,
     state_router,
+    gm_router,
     info_router,
     minigame_router
 ]
