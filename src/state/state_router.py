62c9a967e6da942b86c9bd9f8f4b08b24cd25937
elian118 <elian118@naver.com>
2026-02-04 17:00:25 +0900

    fix: update SessionInfo & add /sessions/user state router

--- src/state/state_router.py
+++ src/state/state_router.py
@@ -1,11 +1,12 @@
 from typing import List
+from jose import jwt
 
 from fastapi import APIRouter, Depends
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from fastapi_utils.cbv import cbv
 
 from common.dtos.wrapped_response import WrappedResponse
-from configs.setting import STATE_MANAGER_URL
+from configs.setting import STATE_MANAGER_URL, SECRET_KEY, ALGORITHM
 from state.dtos.state_dtos import FullPlayerState, SessionInfo, SequenceDetailInfo
 from utils.proxy_request import proxy_request
 
@@ -18,6 +19,7 @@ class StateRouter:
     base_prefix = "/state"
 
     # Todo: 사용자 게임 세션 생성 - state manager api 추가 확인 후 작업
+    # 사용자 게임 세션 생성
     @state_router.post(
         "/session/create", # 임시 라우트 경로
         response_model=WrappedResponse[SessionInfo],
@@ -26,12 +28,31 @@ class StateRouter:
     async def add_session(self, auth: HTTPAuthorizationCredentials = Depends(security)):
         return await proxy_request("POST", STATE_MANAGER_URL, f"{self.base_prefix}/session/create", auth.credentials)
 
+
+    # 사용자 세션 목록 조회
+    @state_router.get(
+        "/sessions/user",
+        response_model = WrappedResponse[List[SessionInfo]],
+        summary="사용자 활성 세션 목록을 조회합니다."
+    )
+    async def get_sessions_by_user_id(self, auth: HTTPAuthorizationCredentials = Depends(security)):
+        token = auth.credentials
+        # 1. 토큰 복호화 및 유효성 검증
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: str = payload.get("sub")
+
+        return await proxy_request(
+            "GET",
+            STATE_MANAGER_URL,
+        f"{self.base_prefix}/sessions/user/{user_id}",
+            auth.credentials
+        )
+
     @state_router.get(
         "/sessions/active",
         response_model=WrappedResponse[List[SessionInfo]],
-        summary="사용자의 활성화 세션 목록을 조회합니다.(수정중)"
+        summary="전체 활성화 세션 목록을 조회합니다.(FE에서 불필요해 보임)"
     )
-    # Fixme: 황성화 세션 목록 조회 - user_id 추가 전달(state manager api request 모델 새로 추가 예정)
     async def get_sessions(self, auth: HTTPAuthorizationCredentials = Depends(security)):
         return await proxy_request("GET", STATE_MANAGER_URL, f"{self.base_prefix}/sessions/active", auth.credentials)
 
